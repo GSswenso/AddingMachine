830bb283d4835640cf5967cfdcbb997c232acec3
Gerik Swenson <gsswenso@asu.edu>
2020-10-05 20:33:05 -0700

    First commit

---
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,41 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+  private int total;
+  
+  public AddingMachine () 
+  {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () 
+  {
+    return 0;
+  }
+  /*This method adds to total
+   * @param value is the value added
+   */
+  public void add (int value) 
+  {
+	  
+  }
+  
+  /*This method subtracts from total
+   * @param value is the value subtracted
+   */
+  public void subtract (int value)
+  {
+	  
+  }
+
+  public String toString () 
+  {
+    return "";
+  }
+
+  public void clear() 
+  {
+	  
+  }
+}
